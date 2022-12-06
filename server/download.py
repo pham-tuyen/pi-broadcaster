7cfbdb762ce29e24bc21d00d5198ec4f4b559157
pham-tuyen <phamthanhtuyen2k8@gmail.com>
2022-12-06 14:59:32 +0700

    Update download.py

--- server/download.py
+++ server/download.py
@@ -1,6 +1,5 @@
-from __future__ import unicode_literals
-import requests, youtube_dl, os, json, platform
-from datetime import datetime
+import re, os, json
+from pytube import Playlist
 
 f = open(".config", "r")
 config = f.read()
@@ -11,44 +10,16 @@ for i in range(0, len(config)):
     if not os.path.isdir(config[i]["dir"]):
         os.mkdir(config[i]["dir"])
 
-api = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&key=AIzaSyD9SzNDbCbjGG_S-3bVVHWHMjMq1qmfV9A&playlistId="
-data = []
-for i in range(0, len(config)):
-    data.append(json.loads(requests.get(api + config[i]["id"]).text))
+playlist = Playlist(config[i]["url"])
+playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
 
-def download(id, name, dir):
-    ytdl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': "/" + dir + "/" + name + ".%(ext)s"
-    }
-    try:
-        with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
-            ydl.download(["https://youtube.com/watch?v=" + id])
-    except:
-        pass
-    if platform.system() == 'Windows':
-        os.system("del " + dir + "/" + name + ".%(ext)s")
-    else:
-        os.system("rm " + dir + "/" + name + ".%(ext)s")
-    os.system("7z a " + dir + "/" + name + ".zip " + dir + "/" + name + ".mp3")
-    if platform.system() == 'Windows':
-        os.system("del " + dir + "/" + name + ".mp3")
-    else:
-        os.system("rm " + dir + "/" + name + ".mp3")
-    
-while True:
-    for i in range(0, len(config)):
-        now = datetime.now()
-        if now.hour * 3600 + now.minute * 60 + now.second <= config[i]["time"] * 3600:
-            while True:
-                download(data[i]["items"][len(os.listdir(config[i]["dir"]))]["snippet"]["resourceId"]["videoId"], str(len(os.listdir(config[i]["dir"]))), config[i]["dir"])
-                now = datetime.now()
-                if data[i]["nextPageToken"] == "" or now.hour * 3600 + now.minute * 60 + now.second >= config[i]["time"] * 3600:
-                    break
-                data[i] = json.loads(requests.get(api + config[i]["id"] + "&pageToken=" + data[i]["nextPageToken"]).text)
-            
+for index in range(0, len(config)):
+    for i in range(len(os.listdir(config[index]["dir"])), len(playlist.videos)):
+        audioStream = playlist.videos[i].streams.get_by_itag("140")
+        audioStream.download(output_path=config[index]["dir"], filename=str(i) + ".mp4")
+        os.chdir(config[index]["dir"])  
+        os.system("ffmpeg -i " + str(i) + ".mp4 " + str(i) + ".mp3")
+        os.remove(str(i) + ".mp4")
+        os.system("7z a " + str(i) + ".zip " + str(i) + ".mp3")
+        os.remove(str(i) + ".mp3")
+        os.chdir("..")
